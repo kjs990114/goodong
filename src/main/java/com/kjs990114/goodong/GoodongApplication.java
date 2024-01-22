d9bac0fc7f590ad81024c5bade6201df97c16303
김준성 <keemjoonsung@gimjunseong-ui-MacBookAir.local>
2024-01-22 20:30:34 +0900

    Initial commit

---
+++ src/main/java/com/kjs990114/goodong/GoodongApplication.java
@@ -0,0 +1,13 @@
+package com.kjs990114.goodong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjs990114/goodong/GoodongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjs990114.goodong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
